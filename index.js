93647e3d782e598201d4167cd475ef440006a173
Victoro101 <104597517+Victoro101@users.noreply.github.com>
2022-11-01 16:37:40 +0100

    updated files

--- index.js
+++ index.js
@@ -76,6 +76,95 @@ function buttonAnimation(currentKey) {
 
   setTimeout(function () {
     activeButton.classList.remove("pressed");
-  }, 1000);
+  }, 200);
 
 }
+
+setTimeout(() => {
+
+// window.addEventListener("load",function () {
+  var preloader=document.getElementById("preloader");
+  preloader.style.display="none";
+  
+// })
+  
+}, 8000);
+
+
+// Wrap every letter in a span
+var textWrapper = document.querySelectorAll('.ml6 .letters');
+// var textWrapper1=document.querySelector(".m16 .letters");
+textWrapper.forEach(el => {
+  el.innerHTML = el.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+  // textWrapper1.innerHTML = textWrapper1.textContent.replace(/\S/g, "<span class='letterr'>$&</span>");
+  // var textWrapper1 = document.querySelector('.ml6 .letters1');
+  // textWrapper1.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter1'>$&</span>");
+  
+  anime.timeline({loop: true})
+    .add({
+      targets: '.ml6 .letter',
+      translateY: ["1.1em", 0],
+      translateZ: 0,
+      duration: 750,
+      delay: (el, i) => 50 * i
+    }).add({
+      targets: '.ml6',
+      opacity: 0,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1000
+    })
+  
+
+
+
+
+});
+
+//for logo animation
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml8 .circle-white',
+    scale: [0, 3],
+    opacity: [1, 0],
+    easing: "easeInOutExpo",
+    rotateZ: 360,
+    duration: 1100
+  }).add({
+    targets: '.ml8 .circle-container',
+    scale: [0, 1],
+    duration: 1100,
+    easing: "easeInOutExpo",
+    offset: '-=1000'
+  }).add({
+    targets: '.ml8 .circle-dark',
+    scale: [0, 1],
+    duration: 1100,
+    easing: "easeOutExpo",
+    offset: '-=600'
+  }).add({
+    targets: '.ml8 .letters-left',
+    scale: [0, 1],
+    duration: 1200,
+    offset: '-=550'
+  }).add({
+    targets: '.ml8 .bang',
+    scale: [0, 1],
+    rotateZ: [45, 15],
+    duration: 1200,
+    offset: '-=1000'
+  }).add({
+    targets: '.ml8',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1400
+  });
+
+anime({
+  targets: '.ml8 .circle-dark-dashed',
+  rotateZ: 90,
+  duration: 2000,
+  easing: "linear",
+  loop: true
+});
